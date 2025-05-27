cd531db3efb297cad4cc4013409eb124511b6aef
Gabriel Flazao <gabrielflazaopoletti1502@gmail.com>
2025-05-26 22:19:52 -0300

    Resolvendo alguns problemas

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,11 @@
 <template>
-    <h1 class="bg-blue-600">Ola mundo</h1>
-</template>e
+    <main class="bg-blue-600">
+        <div class="flex justify-between">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="size-10">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+            <h1 class="text-center p-4">ECOMMERCE</h1>
+        </div>
+    </main>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,5 @@
 <template>
-    <h1 class="bg-emerald-700">ASFASDFADSD</h1>
+    <main class="bg-gray-200 h-screen">
+        <h1 class="text-4xl text-center p-10">HELLO</h1>
+    </main>
 </template>
